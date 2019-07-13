67b090e70b377f241467b6b06b6987fabccb02f0
Grimturs <dzhumandzhi@gmail.com>
2019-07-13 15:32:13 +0300

    easy add

---
+++ lesson5/lesson5_easy.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+
+# Задача-1:
+# Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
+# из которой запущен данный скрипт.
+# И второй скрипт, удаляющий эти папки.
+
+try:
+    for i in range(1, 10):
+        os.mkdir(f'dir_{i}')
+except FileExistsError:
+    print('Создать папки не удалось')
+else:
+    print('Папки успешно созданы')
+
+try:
+    for i in range(1, 10):
+        os.rmdir(f'dir_{i}')
+except FileNotFoundError:
+    print('Папки для удаления не найдены')
+else:
+    print('Папки успешно удалены')
+
+# Задача-2:
+# Напишите скрипт, отображающий папки текущей директории.
+
+for d in os.listdir(os.getcwd()):
+    if os.path.isdir(d):
+        print(os.path.basename(d))
+    # print(os.path.basename(d) if os.path.isdir(os.path.basename(d)) else f'{d} - не является папкой')
+
+# Задача-3:
+# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+shutil.copyfile(__file__, f'{__file__}_copy.py')
